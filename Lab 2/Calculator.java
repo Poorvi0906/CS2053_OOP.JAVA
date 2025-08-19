ddc69273a360eabad3404255bae263eeb453c030
Poorvi0906 <poorvirbca24@rvu.edu.in>
2025-08-19 10:55:32 +0530

    Create Calculator.java

---
+++ Lab 2/Calculator.java
@@ -0,0 +1,15 @@
+package program;
+
+public class Calculator {
+	int add(int a, int b) {
+		return a+b;
+	}
+	int subtract(int a, int b) {
+		return a-b;
+	}
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		System.out.println("Addition:" + calc.add(10, 5));
+		System.out.println("Substraction:" + calc.subtract(10, 5));
+	}
+}
