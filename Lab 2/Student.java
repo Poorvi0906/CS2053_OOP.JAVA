c9f79603bc33b8b64135b22f7eb3f7222b3b59c9
Poorvi0906 <poorvirbca24@rvu.edu.in>
2025-08-19 10:46:35 +0530

    Create Student.java

---
+++ Lab 2/Student.java
@@ -0,0 +1,19 @@
+package program;
+
+public class Student 
+{
+	String name;
+	int age;
+	
+	void displayInfo() 
+	{
+		System.out.println("Name:"+ name +", Age:"+age); 
+		}
+public static void main(String[] args)	
+	{
+		Student s1 = new Student();
+		s1.name ="john";
+		s1.age =20;
+		s1.displayInfo();
+	}
+}
